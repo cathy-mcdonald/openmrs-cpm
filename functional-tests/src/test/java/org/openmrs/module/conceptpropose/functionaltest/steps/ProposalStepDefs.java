6b4e2ed0bab2112714392397289c0b546275d4b8
cathy-mcdonald <github.cathy_mcdonald@spamgourmet.com>
2014-02-11 14:57:59 +1100

    Populate functions for editing proposal

--- functional-tests/src/test/java/org/openmrs/module/conceptpropose/functionaltest/steps/ProposalStepDefs.java
+++ functional-tests/src/test/java/org/openmrs/module/conceptpropose/functionaltest/steps/ProposalStepDefs.java
@@ -5,9 +5,8 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.openmrs.module.conceptpropose.pagemodel.AdminPage;
 import org.openmrs.module.conceptpropose.pagemodel.CreateProposalPage;
+import org.openmrs.module.conceptpropose.pagemodel.EditProposalPage;
 import org.openmrs.module.conceptpropose.pagemodel.MonitorProposalsPage;
-import org.openmrs.module.conceptpropose.pagemodel.SettingsPage;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.io.IOException;
 
@@ -18,10 +17,14 @@ public class ProposalStepDefs {
     private CreateProposalPage createProposalPage;
     private MonitorProposalsPage monitorProposalsPage;
     private AdminPage adminPage;
+    private EditProposalPage editProposalPage;
 
     @Given("^I have a saved draft proposal$")
-     public void navigate_to_page() throws IOException {
+    public void navigate_to_page() throws IOException, InterruptedException {
+        loadProposalMonitorPage();
 
+        editProposalPage = monitorProposalsPage.navigateToEditLastProposalLink();
+        assert(editProposalPage.showsDraftProposal());
     }
 
     @When("^I submit the proposal$")
@@ -37,30 +40,52 @@ public class ProposalStepDefs {
 
     @Given("^I have a new proposal with all necessary details$")
      public void fill_a_new_proposal() throws IOException {
-        loadPage();
+        loadCreateProposalPage();
         createProposalPage.enterNewProposal("Some Name","email@example.com","Some Comments");
     }
 
     @When("^I save$")
-    public void save_new_proposal(){
+    public void save_new_proposal() throws InterruptedException{
         createProposalPage.saveNewProposal();
+        Thread.sleep(500);
     }
 
     @Then("^the proposal is stored with the details$")
     public void check_the_details() throws InterruptedException, IOException {
         loadProposalMonitorPage();
-        assertThat(monitorProposalsPage.getLastProposalName(), equalTo("Some Name"));
-        assertThat(monitorProposalsPage.getLastProposalDescription(), equalTo("Some Comments"));
+        assertThat(monitorProposalsPage.getLastDraftProposalName(), equalTo("Some Name"));
+        assertThat(monitorProposalsPage.getLastDraftProposalDescription(), equalTo("Some Comments"));
+    }
+
+    @When("^I change the details and save$")
+    public void change_details_and_save_proposal() throws InterruptedException {
+        editProposalPage.enterNewName("New Name");
+        editProposalPage.saveChanges();
+        Thread.sleep(500);
+    }
+
+    @Then("^the proposal is stored with the new details$")
+    public void check_the_new_details() throws InterruptedException, IOException {
+        loadProposalMonitorPage();
+        assertThat(monitorProposalsPage.getLastDraftProposalName(), equalTo("New Name"));
+    }
+
+    public void loadAdminPage() throws IOException {
+        Login login = new Login();
+        adminPage = login.login();
+    }
+
+    private void loadCreateProposalPage() throws IOException {
+        if (adminPage == null)
+            loadAdminPage();
+        createProposalPage= adminPage.navigateToCreateProposalPage();
     }
 
     private void loadProposalMonitorPage() throws IOException {
+        if (adminPage == null)
+            loadAdminPage();
         monitorProposalsPage = adminPage.navigateToMonitorProposals();
     }
 
 
-    public void loadPage() throws IOException {
-        Login login = new Login();
-        adminPage = login.login();
-        createProposalPage= adminPage.navigateToCreateProposalPage();
-    }
 }

a74d21f0756b57a20254ac9d646733b1079d4fe4
cathy-mcdonald <github.cathy_mcdonald@spamgourmet.com>
2014-02-11 15:05:21 +1100

    Add functionality for navigating to edit proposal page

--- functional-tests/src/test/java/org/openmrs/module/conceptpropose/pagemodel/MonitorProposalsPage.java
+++ functional-tests/src/test/java/org/openmrs/module/conceptpropose/pagemodel/MonitorProposalsPage.java
@@ -8,31 +8,43 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import java.util.List;
 
 public class MonitorProposalsPage extends BaseCpmPage {
+
     private By resultsSelector = By.className("results");
+
     public MonitorProposalsPage(WebDriver driver) {
         super(driver);
     }
 
-    public String getLastProposalName(){
-        defaultWait.until(new ExpectedCondition<Boolean>() {
-            public Boolean apply(WebDriver input) {
-                return input.findElements(resultsSelector).size() > 0;
-            }
-        });
-
-        List <WebElement> resultRowsElement = driver.findElements(By.className("ng-scope"));
-        WebElement lastRowOfResults =  resultRowsElement.get(resultRowsElement.size()-1);
-        return lastRowOfResults.findElements(By.className("ng-binding")).get(0).getText();
+    public String getLastDraftProposalName() throws InterruptedException {
+        return getLastDraftProposalRow().findElements(By.className("ng-binding")).get(0).getText();
     }
-    public String getLastProposalDescription(){
+
+    public String getLastDraftProposalDescription() throws InterruptedException {
+         return getLastDraftProposalRow().findElements(By.className("ng-binding")).get(1).getText();
+    }
+
+    public EditProposalPage navigateToEditLastProposalLink() throws InterruptedException {
+        getLastDraftProposalRow().click();
+        return new EditProposalPage(driver);
+    }
+
+    // Returns row for proposal closest to bottom of list with "Draft" in Status column
+    private WebElement getLastDraftProposalRow() throws InterruptedException {
         defaultWait.until(new ExpectedCondition<Boolean>() {
             public Boolean apply(WebDriver input) {
                 return input.findElements(resultsSelector).size() > 0;
             }
         });
+        Thread.sleep(100);
 
         List <WebElement> resultRowsElement = driver.findElements(By.className("ng-scope"));
-        WebElement lastRowOfResults =  resultRowsElement.get(resultRowsElement.size()-1);
-        return lastRowOfResults.findElements(By.className("ng-binding")).get(1).getText();
+        WebElement lastDraftProposalRow = null;
+        for (int i = resultRowsElement.size()-1; (i >= 0) && (lastDraftProposalRow == null); i--) {
+            WebElement resultRow =  resultRowsElement.get(i);
+            if (resultRow.findElements(By.className("ng-binding")).get(3).getText().equals("Draft"))
+                lastDraftProposalRow = resultRow;
+        }
+
+        return lastDraftProposalRow;
     }
 }
